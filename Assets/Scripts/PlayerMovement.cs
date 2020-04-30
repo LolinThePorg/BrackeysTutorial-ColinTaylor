08d6f486f9bae02615bac56fd7404a00b1f1f13f
Colin Taylor <taylc285@hsd.k12.or.us>
2020-04-29 20:41:38 -0700

    all remaining tutorials completed. Sorry i forgot to commit after each one.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    public float upwardsForce = 200f;
+
+    // Use FixedUpdate for physics 
+    void FixedUpdate()
+    {
+        //adding forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); 
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+       
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, upwardsForce, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
