cc4e36182f4c063aa7b795b9580acf82df603250
Colin Taylor <taylc285@hsd.k12.or.us>
2020-05-11 15:29:13 -0700

    added bounce pad

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float bounceForce;
+    public Rigidbody rb;
+    public JumpInput jumpInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnTriggerEnter()
+    {
+        rb.AddForce(new Vector3(0, bounceForce, 0), ForceMode.Impulse);
+        jumpInput.onGround = false;
+    }
+}
